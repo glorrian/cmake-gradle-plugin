664279e712f0e5bb09339109470f71a32e58be41
GloRRian55 <GloRRian55@yandex.ru>
2024-03-18 02:34:16 +0300

    upgrade executor class

--- src/main/java/dev/infochem/cmakegradleplugin/CMakeExecutor.java
+++ src/main/java/dev/infochem/cmakegradleplugin/CMakeExecutor.java
@@ -23,41 +23,44 @@ public class CMakeExecutor {
      * Accepts an argument to define the task for which the command will be executed.
      * The class object is used only for logging
      *
-     * @param _execClass {@link java.lang.Class} label for logger messages
+     * @param execClass {@link java.lang.Class} label for logger messages
      */
-    public CMakeExecutor(Class<?> _execClass) {
-        PREFIX = "[%s EXECUTOR] ".formatted(_execClass.getSimpleName());
+    public CMakeExecutor(Class<?> execClass) {
+        PREFIX = "[%s EXECUTOR] ".formatted(execClass.getSimpleName());
     }
 
     /**
      * A method for executing a command in a specific build directory.
      *
-     * @param cmdLine A {@link java.util.List} containing command line arguments. Each argument must be defined in a separate list item. Spaces inside arguments are not allowed
-     * @param workingDir A {@link java.io.File} of the directory where the commands are executed
+     * @param cmdLine List containing command line arguments. Each argument must be defined in a separate list item. Spaces inside arguments are not allowed
+     * @param workingDir Directory where the commands are executed
      */
-    public void execute(final List<String> cmdLine, final File workingDir) {
+    public void execute(List<String> cmdLine, File workingDir) {
+        execute(cmdLine, workingDir, logger::info, logger::error);
+    }
+
+    /**
+     * A method for executing a command in a specific build directory with custom print functions.
+     *
+     * @param cmdLine List containing command line arguments. Each argument must be defined in a separate list item. Spaces inside arguments are not allowed
+     * @param workingDir Directory where the commands are executed
+     * @param inputPrintAction Function for printing the output of the execution
+     * @param errorPrintAction Function for printing output of execution errors
+     */
+    public void execute(final List<String> cmdLine, final File workingDir,
+                        Consumer<? super String> inputPrintAction, Consumer<? super String> errorPrintAction) {
         if (!workingDir.exists() || !workingDir.isDirectory())
             throw new IllegalArgumentException(PREFIX + "The provided working directory is not a valid directory.");
-
         ProcessBuilder processBuilder = new ProcessBuilder(cmdLine);
-        logger.debug("{}Setup ProcessBuilder", PREFIX);
-        logger.warn("{}Setup ProcessBuilder with \"{}\" command", PREFIX, cmdLine);
         processBuilder.directory(workingDir);
-        logger.debug("{}Setup directory(\"{}\") to ProcessBuilder", PREFIX, workingDir.getAbsolutePath());
-
+        logger.debug(PREFIX + "Setup ProcessBuilder with \"{}\" command in {} directory", cmdLine, workingDir.getAbsolutePath());
         try {
             Process process = processBuilder.start();
-            logger.info("{}Starting Process - {}", PREFIX, process);
-            ThreadPoolExecutor executorService = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
-            Future<Void> inputStreamFuture = executorService.submit(new StreamPrintTask(process.getInputStream(), logger::info));
-            Future<Void> errorStreamFuture = executorService.submit(new StreamPrintTask(process.getErrorStream(), logger::error));
+            logger.info(PREFIX + "Starting Process - {}", process);
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
+            printStream(executorService, new StreamPrintService(process.getInputStream(), inputPrintAction));
+            printStream(executorService, new StreamPrintService(process.getInputStream(), errorPrintAction));
             int exitCode = process.waitFor();
-            try {
-                inputStreamFuture.get(3, TimeUnit.SECONDS);
-                errorStreamFuture.get(3, TimeUnit.SECONDS);
-            } catch (ExecutionException | TimeoutException e) {
-                logger.error(PREFIX + "Timed out waiting for InputStream to be closed.");
-            }
             if (exitCode != 0) {
                 throw new GradleException(PREFIX + "Command execution failed with an error(return code %d)".formatted(exitCode));
             }
@@ -66,28 +69,39 @@ public class CMakeExecutor {
         }
     }
 
+    private void printStream(ExecutorService executorService, StreamPrintService printService) {
+        Future<Void> streamFuture = executorService.submit(printService);
+        throwIfTimeOut(streamFuture);
+    }
+
+    private void throwIfTimeOut(Future<?> future) {
+        try {
+            future.get(3, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+            logger.warn(PREFIX + "Timed out waiting for input stream to be closed.");
+        }
+    }
 
     /**
      * A callable class for use in {@link java.util.concurrent.ThreadPoolExecutor}
      * Used to print information from input streams (Basic input stream and error input stream)
      */
-    private static class StreamPrintTask implements Callable<Void> {
+    public static class StreamPrintService implements Callable<Void> {
         private final InputStream stream;
         private final Consumer<? super String> executable;
 
         /**
          * Accepts an input stream and a function to print this stream
-         * @param _stream {@link java.io.InputStream} provides information from command execution
-         * @param _executable A function with {@link java.util.function.Consumer} type to print information from {@link java.io.InputStream}
+         * @param stream {@link java.io.InputStream} provides information from command execution
+         * @param executable A function with {@link java.util.function.Consumer} type to print information from {@link java.io.InputStream}
          */
-        public StreamPrintTask(InputStream _stream, Consumer<? super String> _executable) {
-            stream = _stream;
-            executable = _executable;
+        public StreamPrintService(InputStream stream, Consumer<? super String> executable) {
+            this.stream = stream;
+            this.executable = executable;
         }
 
         /**
          * Method for using in {@link java.util.concurrent.ThreadPoolExecutor#submit(Callable)  }
-         * @return {@link java.lang.Void}
          */
         @Override
         public Void call() {
@@ -95,11 +109,6 @@ public class CMakeExecutor {
             return null;
         }
 
-        /**
-         * Print all information from {@link java.io.InputStream} using {@link java.io.BufferedReader}
-         * @param inputStream {@link java.io.InputStream} provides information from command execution
-         * @param exec A function with {@link java.util.function.Consumer} type to print information from {@link java.io.InputStream}
-         */
         private void executeStream(InputStream inputStream, Consumer<? super String> exec) {
             Stream<String> inputLines = new BufferedReader(new InputStreamReader(inputStream)).lines();
             inputLines.forEach(exec);
